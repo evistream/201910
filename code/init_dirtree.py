98e0c2708b9d01f5ff9479ffc0e515f30889920c
evistream <evistream@yahoo.co.jp>
2019-11-01 10:36:29 +0900

    dirtree

---
+++ code/init_dirtree.py
@@ -0,0 +1,20 @@
+import os
+
+TEMP_DIRS=[
+    'config/{}',
+    'data/{}/scan',
+    'data/{}/selectelectrode',
+    'data/{}/raw',
+    'data/{}/spike',
+    'data/{}/dataset',
+]
+
+def makedirs_(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def render(exps):
+    for exp in exps:
+        for dr in TEMP_DIRS:
+            path = dr.format(exp)
+            makedirs_(path)
