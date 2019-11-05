09d18bfc0aa452ebb91fab25ca456954fe8ea3a4
evistream <evistream@yahoo.co.jp>
2019-11-05 09:17:27 +0900

    pushSlack

--- maxone_code/util.py
+++ maxone_code/util.py
@@ -2,3 +2,12 @@
 SIZE_CHANNEL=1024
 SAMPLING_FREQ=20000
 RECORD_GAIN=512
+
+
+import requests
+
+def pushSlack(text='hello'):
+    url = 'https://hooks.slack.com/services/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'
+    headers = {'Content-type': 'application/json'}
+    payload = {'text': text}
+    r = requests.post(url, headers=headers, data=json.dumps(payload))
